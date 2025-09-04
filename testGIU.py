ab3b6e656600103bb8f20c2ef1ff384986b47ef0
Pawel Powroznik <ppowroznik@gmail.com>
2025-09-04 12:41:20 +0200

    Dodanie plików imageloader

--- testGIU.py
+++ testGIU.py
@@ -1,50 +1,5 @@
-import sys
-from PyQt5.QtWidgets import (
-    QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QDialog, QDialogButtonBox, QHBoxLayout,
-)
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
+
 from PyQt5 import QtGui, QtCore
-
-
-# class CustomPopup(QDialog):
-#     def __init__(self, message, parent=None):
-#         super().__init__(parent)
-#         self.setWindowTitle("Mój Popup")
-#         self.setModal(True)
-#
-#         layout = QVBoxLayout()
-#
-#         label = QLabel(message)
-#         label.setAlignment(Qt.AlignCenter)
-#         layout.addWidget(label)
-#
-#         buttons = QDialogButtonBox(QDialogButtonBox.Ok)
-#         buttons.accepted.connect(self.accept)
-#         layout.addWidget(buttons)
-#
-#         self.setLayout(layout)
-#         self.resize(250, 120)
-#
-#         self.setStyleSheet("""
-#                     QDialog {
-#                         background-color: #324151;
-#                         border-radius: 10px;
-#                     }
-#                     QLabel {
-#                         font-size: 14px;
-#                         color: #FFFFFF;
-#                     }
-#                     QPushButton {
-#                         background-color: #e16e38;
-#                         color: white;
-#                         padding: 6px 12px;
-#                         border-radius: 6px;
-#                     }
-#                     QPushButton:hover {
-#                         background-color: #c5744e;
-#                     }
-#                 """)
 
 
 import sys
