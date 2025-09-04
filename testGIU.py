17b40514a2287f9ca6ce66d9f2dbdc01b20c25ac
MSP <MSP>
2025-09-04 12:23:11 +0200

    Load image

--- testGIU.py
+++ testGIU.py
@@ -1,9 +1,10 @@
 import sys
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QDialog, QDialogButtonBox, QHBoxLayout
+    QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QDialog, QDialogButtonBox, QHBoxLayout,
 )
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
+from PyQt5 import QtGui, QtCore
 
 
 # class CustomPopup(QDialog):
@@ -48,7 +49,7 @@ from PyQt5.QtCore import Qt
 
 import sys
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QDialog, QDialogButtonBox, QHBoxLayout
+    QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QDialog, QDialogButtonBox, QHBoxLayout, QFileDialog
 )
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
@@ -77,6 +78,77 @@ class CustomPopup(QDialog):
             QPushButton { background-color: #e16e38; color: white; padding: 6px 12px; border-radius: 6px; }
             QPushButton:hover { background-color: #c5744e; }
         """)
+
+class ImageLoader(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Wczytywanie obrazu z pliku")
+
+        # Layout
+        self.layout = QVBoxLayout()
+
+        # Przycisk do wczytywania obrazu
+        self.load_button = QPushButton("Wczytaj obraz do analizy")
+        self.load_button.clicked.connect(self.load_image)
+        self.load_button.setFixedSize(220, 30)
+        self.load_button.setStyleSheet("""
+                    font-size: 18px;
+                    color: #FFFFFF;
+                    font-weight: bold;
+                    background-color: #e16e38;
+                    border-radius: 6px;
+                """)
+
+        #Tutaj przycisk z informacją
+
+        self.info_button = QPushButton("")
+        self.info_button.setIcon(QtGui.QIcon('inf.png'))
+        self.info_button.setFixedSize(30, 30)
+        self.info_button.clicked.connect(self.show_info)
+        self.info_button.setStyleSheet("""
+                    font-size: 18px;
+                    color: #FFFFFF;
+                    font-weight: bold;
+                    # background-color: #e16e38;
+                    border-radius: 6px;
+                """)
+
+        # Etykieta do wyświetlania obrazu
+        self.image_label = QLabel("Tutaj pojawi się obraz")
+        self.image_label.setScaledContents(True)  # Automatyczne skalowanie obrazu
+        self.image_label.setFixedSize(300, 300)
+        # self.image_label.resize(300, 300)
+
+        # Dodajemy widgety do layoutu
+        self.layout.addWidget(self.load_button)
+        self.layout.addWidget(self.info_button)
+        self.layout.addWidget(self.image_label)
+
+
+        self.setLayout(self.layout)
+        self.resize(300, 300)
+
+    def load_image(self):
+        # Otwieramy okno dialogowe do wyboru pliku
+        file_path, _ = QFileDialog.getOpenFileName(
+            self,
+            "Wybierz obraz do analizy",
+            "",
+            "Obrazy (*.png *.jpg *.jpeg *.bmp *.gif);;Wszystkie pliki (*)"
+        )
+
+        # Jeżeli użytkownik wybrał plik, wczytaj go
+        if file_path:
+            pixmap = QPixmap(file_path)
+            pixmap = pixmap.scaled(300, 300)
+            self.image_label.setPixmap(pixmap)
+        else:
+            self.image_label.setText("Nie wybrano pliku.")
+
+    def show_info(self):
+        pass
+#
+
 
 
 class MyWindow(QWidget):
@@ -122,6 +194,10 @@ class MyWindow(QWidget):
         center_title = QLabel("Środkowy obszar – tu możesz dodawać własne widżety")
         center_title.setStyleSheet("color: #7d7d7d; font-size: 16px;")
         center_title.setAlignment(Qt.AlignHCenter)
+
+        # Image loader
+        image_layout = ImageLoader()
+        center_layout.addWidget(image_layout)
 
         # przykładowy przycisk „?” w środku
         self.button = QPushButton("?")
