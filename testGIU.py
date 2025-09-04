3ac18c018a14082859e64fcc87307e60eb500531
Pawel Powroznik <ppowroznik@gmail.com>
2025-09-04 15:00:30 +0200

    panele na gorze i dole z obrazkami

--- testGIU.py
+++ testGIU.py
@@ -38,22 +38,8 @@ class MyWindow(QWidget):
         image_layout = ImageLoader.ImageLoader()
         center_layout.addWidget(image_layout)
 
-        # przykładowy przycisk „?” w środku
-        self.button = QPushButton("?")
-        self.button.setFixedSize(22, 22)
-        self.button.clicked.connect(self.show_custom_popup)
-        self.button.setStyleSheet("""
-            font-size: 18px;
-            color: #FFFFFF;
-            font-weight: bold;
-            background-color: #e16e38;
-            border-radius: 6px;
-        """)
-
         center_layout.addWidget(center_title, alignment=Qt.AlignTop | Qt.AlignHCenter)
         center_layout.addStretch(1)
-        center_layout.addWidget(self.button, alignment=Qt.AlignCenter)
-        center_layout.addStretch(2)
 
         # klucz: stretch=1 żeby środek wypełniał przestrzeń między top i bottom
         main_layout.addWidget(self.center_widget, 1)
@@ -63,10 +49,6 @@ class MyWindow(QWidget):
         main_layout.addWidget(self.image_panel_widget_bottom)
 
         self.resize(1200, 800)
-
-    def show_custom_popup(self):
-        popup = CustomPopup.CustomPopup("To jest mój własny popup z przyciskiem OK", self)
-        popup.exec_()
 
 
 if __name__ == "__main__":
